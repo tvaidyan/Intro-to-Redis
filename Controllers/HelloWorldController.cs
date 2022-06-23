fcecb6cd46b1e54e3e49f132b897dcefbd53bb86
Tom Vaidyan <tvaidyan@gmail.com>
2022-06-22 20:46:18 -0500

    initial add

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using redis_explorations.Helpers;
+
+namespace redis_explorations.Controllers
+{
+    [ApiController]
+    [Route("greeting")]
+    public class HelloWorldController
+    {
+        private readonly ICacheHelper cacheHelper;
+
+        public HelloWorldController(ICacheHelper cacheHelper)
+        {
+            this.cacheHelper = cacheHelper;
+        }
+
+        [Route("{name}")]
+        public string Get(string name)
+        {
+            if (cacheHelper.TryGetValue<string>(name, out var greeting))
+            {
+                Console.WriteLine($"{name} is already in cache... fetching from there.");
+                return greeting!;
+            }
+            else
+            {
+                Console.WriteLine($"{name} is NOT in cache. Getting greeting from the database.");
+
+                greeting = GetGreetingFromDatabase(name);
+
+                cacheHelper.Set(name, greeting);
+                return greeting;
+            }
+        }
+
+        private string GetGreetingFromDatabase(string name)
+        {
+            // This method is a pretend database interaction
+            // or any other resource intensive operation
+            Thread.Sleep(5000);
+            return $"Hello, {name}! I was created at {DateTime.Now.ToString("HH:mm:ss")}.";
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,27 @@
+using redis_explorations.Helpers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    // load this from external config in a realworld app
+    // Reminder: Port 5050 is the port that we mapped to, when instantiating the
+    // Redis container in Docker
+    options.Configuration = "localhost:5050";
+});
+
+builder.Services.AddScoped<ICacheHelper, CacheHelper>();
+
+// Configure the HTTP request pipeline.
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
